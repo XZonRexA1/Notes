75a009259231203a7633a580c9ac668bd940f0d2
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-12-01 22:32:41 +0600

    responsive design updated

--- client/src/components/Note/Note.jsx
+++ client/src/components/Note/Note.jsx
@@ -127,13 +127,13 @@ const Note = () => {
   };
 
   return (
-    <div className="max-w-md sm:max-w-2xl font-golos  mx-auto my-12 flex flex-col justify-center">
-      <h1 className="max-w-5xl max-auto flex justify-center bg-gradient-to-t from-blue-400 to-blue-500 font-bold text-4xl text-blue-800">
+    <div className="max-w-md sm:max-w-2xl font-golos mx-auto  sm:mx-auto my-12 flex flex-col justify-center">
+      <h1 className="mx-4 sm:max-auto flex justify-center bg-gradient-to-t from-blue-400 to-blue-500 font-bold text-4xl text-blue-800">
         Notes
       </h1>
-      <div className="bg-blue-200 shadow-lg py-4">
+      <div className="bg-blue-200 mx-4 shadow-lg py-4">
         <p className="ml-4 pt-2">*type your notes here</p>
-        <div className="flex">
+        <div className="flex flex-col sm:flex-row">
           <textarea
             type="text"
             className="border-2 p-4 rounded-md m-4 h-12  focus:outline-none"
@@ -142,56 +142,66 @@ const Note = () => {
             required
           />
           <button
-            className="bg-blue-500 px-4 rounded-md font-golos font-semibold  py-2 h-12 mt-4 text-white"
+            className="bg-blue-500 hover:bg-blue-400 px-4 mx-4 sm:mx-0 rounded-md font-golos font-semibold  py-2 h-12 sm:mt-4 text-white"
             onClick={handleSave}
           >
             Save
           </button>
           <Toaster />
         </div>
+
         <ul>
-          {notes?.map((note) => (
+          <p className="ml-4 pt-4 font-semibold">*Your notes:</p>
+          {notes?.map((note, index) => (
             <li key={note._id}>
               {editingNoteId === note._id ? (
                 <>
                   <input
                     type="text"
-                    className="border-2 p-4 rounded-md m-4  h-12  focus:outline-none"
+                    className="border-2 sm:w-none w-40 rounded-md mx-4 my-2 sm:my-0 sm:mt-2  sm:ml-6  p-4 mt-2 focus:outline-none"
                     value={editedNoteText}
                     onChange={(e) => setEditedNoteText(e.target.value)}
                   />
 
-                  <button
-                    className="bg-green-500 px-4 py-2 rounded-md font-extrabold"
-                    onClick={() => handleEditSave(note._id)}
-                  >
-                    Save
-                  </button>
-                  <button
-                    className="bg-red-500 ml-2 px-4 py-2 rounded-md font-extrabold"
-                    onClick={handleEditCancel}
-                  >
-                    Cancel
-                  </button>
+                  <div className="flex gap-x-2 sm:mt-2 ml-4">
+                    <button
+                      className="bg-green-500 hover:bg-green-400 text-white  sm:ml-2 px-4 h-8 rounded-md font-extrabold "
+                      onClick={() => handleEditSave(note._id)}
+                    >
+                      Save
+                    </button>
+
+                    <button
+                      className="bg-red-500 hover:bg-red-400 text-white  sm:ml-2 px-4 h-8 rounded-md font-extrabold"
+                      onClick={handleEditCancel}
+                    >
+                      Cancel
+                    </button>
+                  </div>
                 </>
               ) : (
                 <>
-                  <div className="flex  items-end">
-                    <p className="bg-white  ml-4  rounded p-4 mt-2">
+                  <div className="flex flex-col justify-center  sm:gap-y-0  ">
+                    <p className="ml-4 mt-4 w-8 text-center rounded-md bg-blue-400 text-white ">
+                      {index + 1}{" "}
+                    </p>
+                    <p className="bg-white mx-4 my-2 sm:my-0 sm:mt-2  sm:ml-4  rounded p-4 mt-2">
                       {note.text}
                     </p>
-                    <button
-                      className="text-green-500 bg-white  ml-2 px-4 h-8 rounded-md font-extrabold"
-                      onClick={() => handleEditStart(note._id, note.text)}
-                    >
-                      Edit
-                    </button>
-                    <button
-                      className="text-red-500 mr-2 bg-white ml-2 px-4 h-8 rounded-md font-extrabold"
-                      onClick={() => handleDelete(note._id)}
-                    >
-                      Delete
-                    </button>
+                    <div className="flex gap-x-2 sm:mt-2 justify-end">
+                      <button
+                        className="text-green-500 hover:opacity-80 bg-white sm:ml-2 px-4 h-8 rounded-md font-extrabold"
+                        onClick={() => handleEditStart(note._id, note.text)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="text-red-500 hover:opacity-80 mr-4 bg-white sm:ml-2 px-4 h-8 rounded-md font-extrabold"
+                        onClick={() => handleDelete(note._id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
                   </div>
                 </>
               )}
